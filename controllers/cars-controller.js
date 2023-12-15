219b9616d3c85ca99235aa479489d20b0ad72cd4
VirLg <19virlg@gmail.com>
2023-12-16 01:08:41 +0200

    delete controller

--- controllers/cars-controller.js
+++ controllers/cars-controller.js
@@ -8,5 +8,18 @@ const addCar = async (req, res) => {
   const result = await CarModel.create(req.body);
   return res.json(result);
 };
+export const deleteById = async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    const result = await CarModel.findByIdAndDelete(contactId);
+    if (!result) {
+      throw HttpError(404, 'Not found');
+    }
+    res.json(result);
+  } catch (error) {
+    console.log('result', error.status);
+    next(error);
+  }
+};
 
-export default { getAllCars, addCar };
+export default { getAllCars, addCar, deleteById };
--- routes/cars.js
+++ routes/cars.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import routeCars from '../controllers/cars-controller.js';
-const { getAllCars, addCar } = routeCars;
+const { getAllCars, addCar, deleteById } = routeCars;
 const router = Router();
 
 router.get('/catalog', getAllCars);
 router.post('/add', addCar);
-
+router.delete('/:contactId', deleteById);
 export default router;
 
 // повинні бути такі маршрути: “/” - домашня сторінка з загальним описом послуг, що надає компанія “/catalog” - сторінка, що містить каталог автівок різної комплектації “/favorites” - сторінка з оголошеннями, які були
